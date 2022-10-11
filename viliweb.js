191c10bb7725de882c5788a14195c49a762df3f8
handandkeep <1477792904@qq.com>
2022-10-11 17:24:07 +0800

    update

--- viliweb.js
+++ viliweb.js
@@ -7,21 +7,53 @@ window.onload = function(){
     let movies2 = document.querySelector(".movies");
     let record2 = document.querySelector(".record");
 
+    getNewContent("cartoon")
+
     cartoon1.addEventListener("click", function (){
         cartoon2.style.display = "block";
         movies2.style.display = "None";
         record2.style.display = "None";
+        getNewContent("cartoon")
     })
 
     movies1.addEventListener("click", function (){
         cartoon2.style.display = "None";
         movies2.style.display = "block";
         record2.style.display = "None";
+        getNewContent("movies")
     })
 
     record1.addEventListener("click", function (){
         cartoon2.style.display = "None";
         movies2.style.display = "None";
         record2.style.display = "block";
+        getNewContent("record")
     })
+
+    function getNewContent(videoType, pages=1){
+        let videoTypeObject = {
+            "cartoon": 0,
+            "movies": 1,
+            "record": 2,
+        }
+        let request = new XMLHttpRequest();
+        if (request){
+            request.open("GET", videoType+".txt", true);
+            request.onreadystatechange = function (){
+                if (request.readyState === 4){
+                    let data = request.responseText.split(",")
+                    let ul = document.getElementsByTagName("ul")[videoTypeObject[videoType]]
+                    if (ul.children.length < 10){
+                        for (let i in data){
+                            let newNodeLi = document.createElement("li")
+                            newNodeLi.innerHTML = "<p>" + data[i] + "</p>"
+                            ul.appendChild(newNodeLi)
+                        }
+                    }
+                    // 在这个地方生成相应的li节点
+                }
+            }
+            request.send(null)
+        }
+    }
 }
