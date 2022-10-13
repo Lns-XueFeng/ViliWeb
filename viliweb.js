85bb38a6d8d978092f7d12484d151a2d865ac0ca
handandkeep <1477792904@qq.com>
2022-10-13 22:57:45 +0800

    update

--- viliweb.js
+++ viliweb.js
@@ -10,6 +10,9 @@ window.onload = function(){
     getNewContent("cartoon")
 
     cartoon1.addEventListener("click", function (){
+        let pages = document.querySelector(".pages");
+        pages.setAttribute("data-page", "0");
+
         cartoon2.style.display = "block";
         movies2.style.display = "None";
         record2.style.display = "None";
@@ -17,6 +20,9 @@ window.onload = function(){
     })
 
     movies1.addEventListener("click", function (){
+        let pages = document.querySelector(".pages");
+        pages.setAttribute("data-page", "0");
+
         cartoon2.style.display = "None";
         movies2.style.display = "block";
         record2.style.display = "None";
@@ -24,13 +30,17 @@ window.onload = function(){
     })
 
     record1.addEventListener("click", function (){
+        let pages = document.querySelector(".pages");
+        pages.setAttribute("data-page", "0");
+
         cartoon2.style.display = "None";
         movies2.style.display = "None";
         record2.style.display = "block";
         getNewContent("record")
     })
 
-    function getNewContent(videoType, pages=1){
+    // ajax更新数据
+    function getNewContent(videoType, pages=0){
         let videoTypeObject = {
             "cartoon": 0,
             "movies": 1,
@@ -41,19 +51,49 @@ window.onload = function(){
             request.open("GET", videoType+".txt", true);
             request.onreadystatechange = function (){
                 if (request.readyState === 4){
-                    let data = request.responseText.split(",")
-                    let ul = document.getElementsByTagName("ul")[videoTypeObject[videoType]]
+                    let data = request.responseText.split(",");
+                    if (data.length < pages){
+                        return;
+                    }
+                    let sliceData = data.slice(pages, pages+10);
+                    console.log(sliceData)
+                    let ul = document.getElementsByTagName("ul")[videoTypeObject[videoType]];
                     if (ul.children.length < 10){
-                        for (let i in data){
-                            let newNodeLi = document.createElement("li")
-                            newNodeLi.innerHTML = "<p>" + data[i] + "</p>"
-                            ul.appendChild(newNodeLi)
+                        console.log(1)
+                        for (let i in sliceData){
+                            let newNodeLi = document.createElement("li");
+                            newNodeLi.innerHTML = "<p>" + sliceData[i] + "</p>";
+                            ul.appendChild(newNodeLi);
                         }
                     }
-                    // 在这个地方生成相应的li节点
                 }
             }
             request.send(null)
+        }
+    }
+
+    // 点击下一页的ajax显示
+    let pages = document.querySelector(".pages");
+    pages.onclick = function (){
+        let dataPage = this.getAttribute("data-page");
+        this.setAttribute("data-page", parseInt(dataPage)+10)
+        let nowCount = this.getAttribute("data-page");
+        console.log(nowCount)
+        if (document.defaultView.getComputedStyle(cartoon2, null).display === "block"){
+            let ul = document.querySelectorAll("ul")[0];
+            ul.innerHTML = ""
+            getNewContent("cartoon", parseInt(nowCount));
+        }
+        if (document.defaultView.getComputedStyle(movies2, null).display === "block"){
+            console.log(2323)
+            let ul = document.querySelectorAll("ul")[1];
+            ul.innerHTML = ""
+            getNewContent("movies", parseInt(nowCount));
+        }
+        if (document.defaultView.getComputedStyle(record2, null).display === "block"){
+            let ul = document.querySelectorAll("ul")[2];
+            ul.innerHTML = ""
+            getNewContent("record", parseInt(nowCount));
         }
     }
 }
